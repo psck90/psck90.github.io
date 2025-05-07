86459cf51a9c8c593570f2bf04b35e904d1529b7
piotrhut42 <piotr@hutfortytwo.co.uk>
2025-05-07 08:55:15 +0100

    Configures font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 // src/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Roboto Mono"', 'monospace'],
+        sans: ['"Roboto Mono"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [
+],
 };
 
